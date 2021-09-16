2c2db8ab86240b8698dd99ec2a4dfbf3fe63a32f
Baby David <baby.david.baby@gmail.com>
2021-09-16 12:29:09 +0300

    added transection nonce const, small refactor

--- src/vue/forms/transfer-form/TransferSendAuthorizeStep.vue
+++ src/vue/forms/transfer-form/TransferSendAuthorizeStep.vue
@@ -97,6 +97,7 @@ import { getAccountPair, unlockAccount, lockAccount, cropAddress } from '@/js/he
 import { ErrorHandler } from '@/js/helpers/error-handler'
 import { Bus } from '@/js/helpers/event-bus'
 import { errors } from '@/js/errors'
+import { DEFAULT_TRANSACTION_NONCE } from '@/js/const/transaction-nonce.const'
 
 const EVENTS = {
   submit: 'submit',
@@ -192,7 +193,7 @@ export default {
         try {
           unlockAccount(state.senderPair, formController.form.password.value)
           const options = {
-            nonce: -1,
+            nonce: DEFAULT_TRANSACTION_NONCE,
             tip: formController.form.tipAmount.value,
           }
           const unsubscribe = await props.former.attrs.tx.signAndSend(
