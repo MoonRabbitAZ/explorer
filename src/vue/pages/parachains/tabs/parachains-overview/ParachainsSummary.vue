a8306555a7c404cb4685df55491d0163474acbcd
Baby David <baby.david.baby@gmail.com>
2021-10-06 18:23:40 +0300

    fix parachain summary

--- src/vue/pages/parachains/tabs/parachains-overview/ParachainsSummary.vue
+++ src/vue/pages/parachains/tabs/parachains-overview/ParachainsSummary.vue
@@ -17,9 +17,16 @@
       <info-block
         class="parachains-summary__info"
         :title="$t('parachains-page.parachains-summary.lease-period-header')"
-        :value="leasePeriod?.length?.toString()"
-        :secondary-value="leasePeriod?.progress?.toString()"
-      />
+        :value="leaseTimes?.total"
+        :secondary-value="leaseTimes?.progress"
+      >
+        <template #additional>
+          <progress-bar
+            :current="leasePeriod?.progress"
+            :total="leasePeriod?.length"
+          />
+        </template>
+      </info-block>
 
       <best-number
         class="parachains-summary__info"
@@ -34,16 +41,48 @@
 import InfoBlock from '@/vue/common/InfoBlock'
 import BestNumber from '@/vue/common/info-blocks/BestNumber'
 import EpochTime from '@/vue/common/info-blocks/EpochTime'
+import ProgressBar from '@/vue/common/ProgressBar'
+
+import { useBlockTime } from '@/vue/composables'
+import { computed } from 'vue'
 
 export default {
   name: 'parachains-summary',
 
-  components: { InfoBlock, BestNumber, EpochTime },
+  components: {
+    InfoBlock,
+    BestNumber,
+    EpochTime,
+    ProgressBar,
+  },
 
   props: {
     parachainsAmount: { type: Number, default: 0 },
     parathreadAmount: { type: Number, default: 0 },
     leasePeriod: { type: Object, default: () => ({}) },
+  },
+
+  setup (props) {
+    const { calculateTimeStr } = useBlockTime()
+
+    const leaseTimes = computed(() => {
+      if (props.leasePeriod?.length && props.leasePeriod?.progress) {
+        const progresValue =
+          props.leasePeriod.progress.sub(props.leasePeriod.length)
+
+        return {
+          total: calculateTimeStr(props.leasePeriod.length),
+          progress: calculateTimeStr(progresValue),
+        }
+      } else {
+        return {
+          total: '',
+          progress: '',
+        }
+      }
+    })
+
+    return { leaseTimes }
   },
 }
 </script>
